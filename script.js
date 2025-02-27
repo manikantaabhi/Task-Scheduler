55f3a4a7a932bd863b1902b5b6a1beb1f8b0b450
Murali Koujala <koujala@gmail.com>
2025-02-26 22:34:57 -0500

    Updates

--- script.js
+++ script.js
@@ -1,89 +1,72 @@
-let tasks=[
-    {id:1,description:'This is task one',completed:false},
-    {id:2,description:'This is task two',completed:false},
-    {id:3,description:'This is task Three',completed:false}
-]
-function displayTasks(){
-    const taskContainer=document.querySelector('.task-container');
-    taskContainer.innerHTML='';
-    tasks.forEach(element => {
-        const task=document.createElement('div');
-        task.className='task';
+let tasks = [];
 
-        const leftContainer = document.createElement('div');
-        leftContainer.className='left-container';
-
-        const description=document.createElement('p');
-        description.className='description';
-        description.textContent=element.description;
-
-        leftContainer.appendChild(description);
-
-        task.appendChild(leftContainer);
-
-        const rightContainer = document.createElement('div');
-        rightContainer.className='right-container';
-
-        const editButton=document.createElement('button');
-        editButton.className='action-buttons primary';
-        editButton.id=`edit-task-${element.id}`;
-        editButton.textContent='Edit';
-        editButton.addEventListener('click',()=>
-        {
-            editTask(element.id);
-        });
-
-        const deleteButton=document.createElement('button');
-        deleteButton.className='action-buttons danger';
-        deleteButton.id=`delete-task-${element.id}`;
-        deleteButton.textContent='Delete';
-        deleteButton.addEventListener('click',()=>
-        {
-            deleteTask(element.id);
-        });
-
-        rightContainer.appendChild(editButton);
-        rightContainer.appendChild(deleteButton);
-
-        task.appendChild(rightContainer);
-
-        taskContainer.appendChild(task);
-    });
-
-    function editTask(taskId){
-        alert("edit "+taskId);
-    }
-    function deleteTask(taskId){
-        //alert("delete "+taskId);
-        tasks=tasks.filter(ele=> ele.id!=taskId);
-        displayTasks();
-    }
+let maxTasks = 20;
+//add some dummy data in a loop
+for (let i = 1; i <=maxTasks; i++) {
+    tasks.push({ id: i, description: `This is task ${i}`, completed: false });
 }
 
-displayTasks();
+const taskContainer = document.querySelector('.tasks');
 
-const addBtn=document.getElementById('add-task');
-addBtn.addEventListener('click',()=>
-{
-   const text= document.getElementById('task-input').value;
-    console.log(text);
-    if(text!=''){
-    const task={
-        id:getNewId(),
-        description:`${text}`,
-        completed:false
-    };
-    tasks.push(task);
+// MANI - this is known as even delegation - one event handler only 
+// we check e.target to check what was clicked and trhen grab parent element id)
+taskContainer.addEventListener('click', (e) => {
+    let action = e.target.getAttribute('action');
+    let taskId = parseInt(e.target.parentElement.parentElement.id);
+    if (action === 'edit')
+        editTask(taskId);
+    else if (action === 'delete')
+        deleteTask(taskId);
+});
+
+function editTask(taskId) {
+    alert("edit " + taskId);
+}
+function deleteTask(taskId) {
+    //alert("delete "+taskId);
+    tasks = tasks.filter(ele => ele.id != taskId);
     displayTasks();
-    }   
+}
+
+function displayTasks() {
+    let tasksHtml = ''
+    tasks.forEach(item => {
+        tasksHtml += `
+        <div class="task" id="${item.id}" title='${item.description}'>
+            <div class="left-container">
+                <p class="description">${item.description}</p>
+            </div>
+            <div class="right-container">
+                <button class="action-buttons primary" action="edit">Edit</button>
+                <button class="action-buttons danger" action="delete">Delete</button>
+            </div>
+        </div>`;
+    });
+    taskContainer.innerHTML = tasksHtml;
+}
+
+const addBtn = document.getElementById('add-task');
+addBtn.addEventListener('click', () => {
+    const text = document.getElementById('task-input').value;
+    console.log(text);
+    if (text != '') {
+        const task = {
+            id: getNewId(),
+            description: `${text}`,
+            completed: false
+        };
+        tasks.push(task);
+        displayTasks();
+    }
     else
         alert("description cannot be empty");
 }
 );
 
-function getNewId()
-{
-    if(tasks.length>0)
-        return Math.max(...tasks.map(task=>task.id))+1;
+function getNewId() {
+    if (tasks.length > 0)
+        return Math.max(...tasks.map(task => task.id)) + 1;
     return 1;
 }
+
+displayTasks();
